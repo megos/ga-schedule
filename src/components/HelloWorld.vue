bdf0045ee9754281b3736b05a1b3adf1bf4b7929
megos <gmegos.27@gmail.com>
2018-10-12 23:10:53 +0900

    Revert "Not use dropdown when mobile access"
    This reverts commit a14c8203e70a2ceb5e7d760d676d904be7068dea.

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -100,7 +100,6 @@ import Genetic from 'genetic-js';
 import { HotTable } from '@handsontable/vue';
 import 'handsontable/dist/handsontable.full.min.css';
 import moment from 'moment';
-import isMobile from 'ismobilejs';
 
 const genetic = Genetic.create();
 
@@ -255,12 +254,10 @@ export default {
     change() {
       this.hotSettings.startCols = moment(this.to).diff(this.from, 'days') + 1;
       this.hotSettings.colHeaders = [...Array(this.hotSettings.startCols)].map((a, idx) => moment(this.from).add(idx, 'days').format('MM/DD'));
-      if (!isMobile.any) {
-        this.hotSettings.columns = [...Array(this.hotSettings.startCols)].map(() => ({
-          type: 'dropdown',
-          source: ['○', '×', ''],
-        }));
-      }
+      this.hotSettings.columns = [...Array(this.hotSettings.startCols)].map(() => ({
+        type: 'dropdown',
+        source: ['○', '×', ''],
+      }));
       this.hotSettings.rowHeaders = [...Array(10)].map((a, idx) => `クルー${idx + 1}`);
       this.$refs.hotTableComponent.hotInstance.updateSettings(this.hotSettings);
     },
