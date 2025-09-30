e02c1763ba871d480a2573b605ea5cfd7d9c2f68
megos <gmegos.27@gmail.com>
2025-09-30 21:52:46 +0900

    yarn create vuetify

--- src/App.vue
+++ src/App.vue
@@ -1,40 +1,11 @@
 <template>
   <v-app>
-    <v-toolbar
-      app
-      dark
-      color="primary"
-    >
-      <v-toolbar-title v-text="title" />
-    </v-toolbar>
-    <v-content>
-      <Top />
-    </v-content>
+    <v-main>
+      <router-view />
+    </v-main>
   </v-app>
 </template>
 
-<script>
-import Top from './components/Top'
-
-export default {
-  name: 'App',
-  components: {
-    Top,
-  },
-  data() {
-    return {
-      clipped: false,
-      drawer: true,
-      fixed: false,
-      items: [{
-        icon: 'bubble_chart',
-        title: 'Inspire',
-      }],
-      miniVariant: false,
-      right: true,
-      rightDrawer: false,
-      title: 'てげてげスケジュール',
-    }
-  },
-}
+<script lang="ts" setup>
+  //
 </script>
