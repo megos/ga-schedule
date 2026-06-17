17cf99f0db13e64cf569217339bc6dc1a3e8a061
megos <gmegos.27@gmail.com>
2026-06-17 20:39:48 +0900

    Apply lint (#16)

--- src/components/Top.vue
+++ src/components/Top.vue
@@ -5,8 +5,15 @@
         Step1. 従業員数を入力してください
         <v-card>
           <v-card-text>
-            <v-text-field v-model.number="startRows" :rules="[(value) => 2 <= value && value <= 10 || '2～10人のみ対応です']"
-              type="number" max="10" min="2" label="従業員数" @change="change" />
+            <v-text-field
+              v-model.number="startRows"
+              label="従業員数"
+              max="10"
+              min="2"
+              :rules="[(value) => 2 <= value && value <= 10 || '2～10人のみ対応です']"
+              type="number"
+              @change="change"
+            />
           </v-card-text>
         </v-card>
       </v-col>
@@ -14,8 +21,14 @@
         Step2. 1日に必要な人数を入力してください
         <v-card>
           <v-card-text>
-            <v-text-field v-model.number="needsEmployee" :max="startRows"
-              :rules="[(value) => 1 <= value && value <= 10 || '1～10人のみ対応です']" type="number" min="0" label="必要人数" />
+            <v-text-field
+              v-model.number="needsEmployee"
+              label="必要人数"
+              :max="startRows"
+              min="0"
+              :rules="[(value) => 1 <= value && value <= 10 || '1～10人のみ対応です']"
+              type="number"
+            />
           </v-card-text>
         </v-card>
       </v-col>
@@ -28,8 +41,13 @@
             <h4>開始</h4>
           </v-card-title>
           <v-card-text class="text-xs-center">
-            <v-date-picker v-model="from" :max="to" :allowed-dates="allowDatesFrom" locale="ja-jp"
-              @update:model-value="change" />
+            <v-date-picker
+              v-model="from"
+              :allowed-dates="allowDatesFrom"
+              locale="ja-jp"
+              :max="to"
+              @update:model-value="change"
+            />
           </v-card-text>
         </v-card>
       </v-col>
@@ -39,22 +57,26 @@
             <h4>終了</h4>
           </v-card-title>
           <v-card-text class="text-xs-center">
-            <v-date-picker v-model="to" :min="from" :allowed-dates="allowDatesTo" locale="ja-jp"
-              @update:model-value="change" />
+            <v-date-picker
+              v-model="to"
+              :allowed-dates="allowDatesTo"
+              locale="ja-jp"
+              :min="from"
+              @update:model-value="change"
+            />
           </v-card-text>
         </v-card>
       </v-col>
       <v-col cols="12" md="4" offset-md="4">
         <div>Step4. さあ、スケジュールを作成してみましょう！</div>
-        <v-btn color="info" block @click="generate">
+        <v-btn block color="info" @click="generate">
           スケジュール作成
         </v-btn>
       </v-col>
       <v-col cols="12">
         <div>
           ○：出勤、×：休み
-          <hot-table :settings="hotSettings" ref="tableRef">
-          </hot-table>
+          <hot-table ref="tableRef" :settings="hotSettings" />
         </div>
       </v-col>
       <v-col cols="12">
@@ -71,178 +93,182 @@
 </template>
 
 <script setup lang="ts">
-import { computed, ref, onMounted, type Ref } from 'vue'
-import { useDate } from 'vuetify'
+  import type Handsontable from 'handsontable'
+  import { HotTable } from '@handsontable/vue3'
 
-import { HotTable } from '@handsontable/vue3';
-import { registerAllModules } from 'handsontable/registry';
-import 'handsontable/styles/handsontable.min.css';
-import 'handsontable/styles/ht-theme-main.min.css';
-import Handsontable from 'handsontable';
-// @ts-ignore
-import Genetic from 'genetic-js'
+  // @ts-ignore
+  import Genetic from 'genetic-js'
+  import { registerAllModules } from 'handsontable/registry'
+  import { computed, onMounted, ref, type Ref } from 'vue'
+  import { useDate } from 'vuetify'
+  import { toYYMM } from '../utils'
+  import 'handsontable/styles/handsontable.min.css'
 
-import { toYYMM } from '../utils'
+  import 'handsontable/styles/ht-theme-main.min.css'
 
-// register Handsontable's modules
-registerAllModules();
+  // register Handsontable's modules
+  registerAllModules()
 
-const genetic = Genetic.create()
+  const genetic = Genetic.create()
 
-genetic.optimize = Genetic.Optimize.Minimize
-genetic.select1 = Genetic.Select1.Tournament2
-genetic.select2 = Genetic.Select2.Tournament2
+  genetic.optimize = Genetic.Optimize.Minimize
+  genetic.select1 = Genetic.Select1.Tournament2
+  genetic.select2 = Genetic.Select2.Tournament2
 
-genetic.seed = function () {
-  let text = ''
-  for (let i = 0; i < this.userData.col * this.userData.row; i++) {
-    text += this.userData.charset.charAt(Math.floor(Math.random()
-      * this.userData.charset.length))
-  }
-  return text
-}
-
-genetic.mutate = function (entity: string) {
-  const replaceAt = (str: string, index: number, character: string) => {
-    return str.substr(0, index) + character + str.substr(index + character.length)
+  genetic.seed = function () {
+    let text = ''
+    for (let i = 0; i < this.userData.col * this.userData.row; i++) {
+      text += this.userData.charset.charAt(Math.floor(Math.random()
+        * this.userData.charset.length))
+    }
+    return text
   }
 
-  // chromosomal drift
-  const i = Math.floor(Math.random() * entity.length)
-  return replaceAt(
-    entity,
-    i,
-    this.userData.charset.charAt(Math.floor(Math.random()
-      * this.userData.charset.length)),
-  )
-}
+  genetic.mutate = function (entity: string) {
+    const replaceAt = (str: string, index: number, character: string) => {
+      return str.slice(0, Math.max(0, index)) + character + str.slice(index + character.length)
+    }
 
-genetic.crossover = (mother: string, father: string) => {
-  // two-point crossover
-  const len = mother.length
-  let ca = Math.floor(Math.random() * len)
-  let cb = Math.floor(Math.random() * len)
-  if (ca > cb) {
-    const tmp = cb
-    cb = ca
-    ca = tmp
+    // chromosomal drift
+    const i = Math.floor(Math.random() * entity.length)
+    return replaceAt(
+      entity,
+      i,
+      this.userData.charset.charAt(Math.floor(Math.random()
+        * this.userData.charset.length)),
+    )
   }
 
-  const son = father.substr(0, ca) + mother.substr(ca, cb - ca) + father.substr(cb)
-  const daughter = mother.substr(0, ca) + father.substr(ca, cb - ca) + mother.substr(cb)
+  genetic.crossover = (mother: string, father: string) => {
+    // two-point crossover
+    const len = mother.length
+    let ca = Math.floor(Math.random() * len)
+    let cb = Math.floor(Math.random() * len)
+    if (ca > cb) {
+      const tmp = cb
+      cb = ca
+      ca = tmp
+    }
 
-  return [son, daughter]
-}
+    const son = father.slice(0, Math.max(0, ca)) + mother.slice(ca, cb) + father.slice(cb)
+    const daughter = mother.slice(0, Math.max(0, ca)) + father.slice(ca, cb) + mother.slice(cb)
 
-genetic.fitness = function (entity: string) {
-  let fitness = 0
+    return [son, daughter]
+  }
 
-  for (let col = 0; col < this.userData.col; col++) {
-    let workers = 0
-    for (let row = 0; row < this.userData.row; row++) {
-      const idx = col + (row * this.userData.col)
-      if (col < this.userData.col - 1 && entity[idx] === entity[idx + 1]) {
-        fitness += 1 - this.userData.continuityWeight
+  genetic.fitness = function (entity: string) {
+    let fitness = 0
+
+    for (let col = 0; col < this.userData.col; col++) {
+      let workers = 0
+      for (let row = 0; row < this.userData.row; row++) {
+        const idx = col + (row * this.userData.col)
+        if (col < this.userData.col - 1 && entity[idx] === entity[idx + 1]) {
+          fitness += 1 - this.userData.continuityWeight
+        }
+        workers += (entity[idx] === '○' ? 1 : 0)
       }
-      workers += (entity[idx] === '○' ? 1 : 0)
+      fitness += Math.abs(this.userData.needsEmployee - workers) * this.userData.employeeWeight
     }
-    fitness += Math.abs(this.userData.needsEmployee - workers) * this.userData.employeeWeight
+    return fitness
   }
-  return fitness
-}
 
-genetic.notification = (pop: { fitness: number, entity: string }[], gen: number) => {
-  const value = pop[0]!.entity
-  last.value ??= value
-  if (last.value === value) {
-    return
+  genetic.notification = (pop: { fitness: number, entity: string }[], gen: number) => {
+    const value = pop[0]!.entity
+    last.value ??= value
+    if (last.value === value) {
+      return
+    }
+    last.value = value
+    fitness.value = pop[0]!.fitness
+    tableRef.value?.hotInstance.loadData(Array.from({ length: genetic.userData.row }).map((_, idx) => {
+      const start = idx * genetic.userData.col
+      const end = start + genetic.userData.col
+      return value.slice(start, end).split('')
+    }))
+    generation.value = gen
   }
-  last.value = value
-  fitness.value = pop[0]!.fitness
-  tableRef.value?.hotInstance.loadData([...Array(genetic.userData.row)].map((c, idx) => value.substr(idx * genetic.userData.col, genetic.userData.col).split('')))
-  generation.value = gen
-}
 
-const tableRef = ref<{ hotInstance: Handsontable } | null>(null)
+  const tableRef = ref<{ hotInstance: Handsontable } | null>(null)
 
-const change = () => {
-  const startCols = adapter.getDiff(to.value, from.value, 'days')
-  hotSettings.value.startCols = startCols
-  hotSettings.value.colHeaders = [...new Array(startCols)].map((_, idx) => toYYMM(adapter.addDays(from.value, idx) as Date))
-  hotSettings.value.startRows = startRows.value
-  hotSettings.value.rowHeaders = [...new Array(startRows.value)].map((_, idx) => `クルー${idx + 1}`)
-  tableRef.value?.hotInstance?.updateSettings(hotSettings.value)
-}
-const generate = () => {
-  userData.value.col = hotSettings.value.startCols
-  userData.value.row = hotSettings.value.startRows
-  genetic.evolve(
-    {
-      iterations: 200,
-      size: 100,
-      crossover: 0.3,
-      mutation: 0.3,
-      skip: 20,
+  function change () {
+    const startCols = adapter.getDiff(to.value, from.value, 'days')
+    hotSettings.value.startCols = startCols
+    hotSettings.value.colHeaders = Array.from({ length: startCols }).map((_, idx) => toYYMM(adapter.addDays(from.value, idx) as Date))
+    hotSettings.value.startRows = startRows.value
+    hotSettings.value.rowHeaders = Array.from({ length: startRows.value }).map((_, idx) => `クルー${idx + 1}`)
+    tableRef.value?.hotInstance?.updateSettings(hotSettings.value)
+  }
+  function generate () {
+    userData.value.col = hotSettings.value.startCols
+    userData.value.row = hotSettings.value.startRows
+    genetic.evolve(
+      {
+        iterations: 200,
+        size: 100,
+        crossover: 0.3,
+        mutation: 0.3,
+        skip: 20,
+      },
+      userData.value,
+    )
+  }
+
+  onMounted(change)
+
+  const hotSettings = ref({
+    startRows: 7,
+    startCols: 31,
+    rowHeaders: [] as string[],
+    colHeaders: [] as string[],
+    rowHeaderWidth: 100,
+    columns: Array.from({ length: 31 }).map(() => ({
+      type: 'dropdown',
+      source: ['○', '×', ''],
+    })),
+    height: 'auto',
+    themeName: 'ht-theme-main',
+    licenseKey: 'non-commercial-and-evaluation',
+  }) satisfies Ref<Handsontable.GridSettings>
+
+  const startRows = computed<number, number>({
+    get: () => hotSettings.value.startRows,
+    set: value => {
+      if (value >= 2 && value <= 10) {
+        hotSettings.value.startRows = value
+      }
     },
-    userData.value,
-  )
-}
+  })
 
-onMounted(change)
+  const needsEmployee = computed<number, number>({
+    get: () => userData.value.needsEmployee,
+    set: value => {
+      if (value >= 1 && value <= 10) {
+        userData.value.needsEmployee = value
+      }
+    },
+  })
 
-const hotSettings = ref({
-  startRows: 7,
-  startCols: 31,
-  rowHeaders: [] as string[],
-  colHeaders: [] as string[],
-  rowHeaderWidth: 100,
-  columns: [...new Array(31)].map(() => ({
-    type: 'dropdown',
-    source: ['○', '×', ''],
-  })),
-  height: 'auto',
-  themeName: 'ht-theme-main',
-  licenseKey: 'non-commercial-and-evaluation',
-}) satisfies Ref<Handsontable.GridSettings>
+  const generation = ref(0)
+  const fitness = ref(0)
+  const last = ref<string | null>(null)
 
-const startRows = computed<number, number>({
-  get: () => hotSettings.value.startRows,
-  set: (value) => {
-    if (value >= 2 && value <= 10) {
-      hotSettings.value.startRows = value
-    }
-  },
-})
-
-const needsEmployee = computed<number, number>({
-  get: () => userData.value.needsEmployee,
-  set: (value) => {
-    if (value >= 1 && value <= 10) {
-      userData.value.needsEmployee = value
-    }
-  },
-})
-
-const generation = ref(0)
-const fitness = ref(0)
-const last = ref<string | null>(null)
-
-const today = new Date()
-const adapter = useDate()
-const from = ref(adapter.startOfMonth(today) as Date)
-const to = ref(adapter.endOfMonth(today) as Date)
-const allowDatesFrom = (date: unknown) => {
-  return adapter.getDiff(to.value, date, 'days') <= 31
-}
-const allowDatesTo = (date: unknown) => {
-  return adapter.getDiff(date, from.value, 'days') <= 31
-}
-const userData = ref({
-  needsEmployee: 5,
-  employeeWeight: 0,
-  continuityWeight: 0,
-  charset: '×○',
-  col: 0,
-  row: 0,
-})
+  const today = new Date()
+  const adapter = useDate()
+  const from = ref(adapter.startOfMonth(today) as Date)
+  const to = ref(adapter.endOfMonth(today) as Date)
+  function allowDatesFrom (date: unknown) {
+    return adapter.getDiff(to.value, date, 'days') <= 31
+  }
+  function allowDatesTo (date: unknown) {
+    return adapter.getDiff(date, from.value, 'days') <= 31
+  }
+  const userData = ref({
+    needsEmployee: 5,
+    employeeWeight: 0,
+    continuityWeight: 0,
+    charset: '×○',
+    col: 0,
+    row: 0,
+  })
 </script>
